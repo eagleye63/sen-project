516a47c7c4c295411dc8d8d268003d138c269f17
ashok-g-prajapati <ashokprajapati.ap.app@gmail.com>
2019-03-18 00:53:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+import Signupdoc from './components/Signupdoc';
+import SignUp from './components/SignUp';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <SignUp/>
+      </div>
+    );
+  }
+}
+
+export default App;
