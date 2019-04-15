42e83fc6013d1cbee074d7ab051fa0ebf232b2e4
bravoo84 <bhavikmehta98@gmail.com>
2019-04-15 23:01:30 +0530

    final final temp page

--- src/Components/slots/TempPage.js
+++ src/Components/slots/TempPage.js
@@ -11,6 +11,9 @@ import SendProps from "./SendProps";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from '../navigationbar'
 import DatePicker from 'react-custom-date-picker'
+import {Spinner} from 'reactstrap'
+import { CLIENT_RENEG_LIMIT } from "tls";
+
 
 
 
@@ -23,9 +26,31 @@ class TempPage extends Component {
     this.state = ({
         date:new Date(),
         bookSlot:false,
+        dataAvailable:false,
     });
     this.timeGone=false;
 
+    }
+
+    componentDidMount=()=>{
+        let doctor=firebase.database().ref('/clinic').child(this.props.computedMatch.params.id+"");
+        doctor.once("value").then(snapshot=>{
+            const val =snapshot.val();
+            this.clinicname=val.clinicname;
+            this.doctor=val.doctor;
+            this.specialist=val.specialist;
+            this.degree=val.degree;
+            this.age=val.age;
+            this.phone=val.phone;
+            this.clinicfees=val.clinicfees;
+            this.street=val.street;
+            this.area=val.area;
+            this.city=val.city;
+            console.log("data received is "+this.clinicname);
+            this.setState({
+                dataAvailable:true
+            })
+        })
     }
 
     onChange = (date) =>{
@@ -74,16 +99,25 @@ class TempPage extends Component {
 
 
   render() {
-      console.log(this.state.bookSlot);
+      console.log(this.state.dataAvailable+"   statedataavilable ");
       console.log(' in temp page doctor key '+  this.props.computedMatch.params.id);
       console.log('in temppage patient key '+this.props.patientId);
       var link='https://www.google.com/maps/place/Nidhi+Multispeciality+Hospital/@23.0401044,72.5592146,17z/data=!3m1!4b1!4m15!1m7!3m6!1s0x395e84f521440d4b:0x6853ee97a9a2996b!2sNavrangpura,+Ahmedabad,+Gujarat!3b1!8m2!3d23.0365437!4d72.5611395!3m6!1s0x395e848c996426c3:0x915350ab1da7fe78!8m2!3d23.0400999!4d72.5614035!9m1!1b1'
     /*  Props of doctorName and other doctor details  will be send by Parent Page */
-    return (
 
-      <div> 
-        { !this.state.bookSlot && 
-            <React.Fragment>
+    if(this.state.bookSlot){
+
+        return(
+            <SendProps doctorName={this.props.computedMatch.params.id} patientId={this.props.patientId} searchDate={this.state.date}
+            timeGone={this.timeGone}
+        />
+        )
+    }
+    else if(this.state.dataAvailable){
+
+        return(
+            <div>
+                <React.Fragment>
             <NavigationBar/>
 
             
@@ -114,17 +148,16 @@ class TempPage extends Component {
                 <div className='list-group' style={{border:"3px solid grey"}}>
                 <div className='list-group-item' style={{backgroundColor:'#f1f1f1'}}>
                 <h2 style={{textAlign:"center"}}>Clinic Details</h2>
-                <h4 style={{fontSize:'14px',marginTop:'1%'}}><b>Clinic Name: </b>{/*data.doctor*/}</h4> 
-                                <h4 style={{fontSize:'14px'}}><b>Doctor Name: </b>{/*data.doctor*/}</h4> 
-                                <h4 style={{fontSize:'14px'}}><b>Specialist: </b>{/*data.doctor*/}</h4>
-                                <h4 style={{fontSize:'14px'}}><b>Degree: </b>{/*data.doctor*/}</h4>
-                                <h4 style={{fontSize:'14px'}}><b>Gender: </b>{/*data.doctor*/}</h4>
-                                <h4 style={{fontSize:'14px'}}><b>Age: </b>{/*data.doctor*/}</h4>
-                                <h4 style={{fontSize:'14px'}}><b>Phone: </b>{/*data.doctor*/}</h4>                                
-                                <h4 style={{fontSize:'14px'}}><b>Clinicfees: </b>{/*data.doctor*/}</h4>
-                                <h4 style={{fontSize:'14px'}}><b>Street: </b>{/*data.doctor*/} </h4>
-                                <h4 style={{fontSize:'14px'}}><b>Area: </b>{/*data.doctor*/} </h4>
-                                <h4 style={{fontSize:'14px'}}><b>City: </b>{/*data.doctor*/}</h4>
+                <h4 style={{fontSize:'14px',marginTop:'1%'}}><b>Clinic Name: </b>{this.clinicname}</h4> 
+                                <h4 style={{fontSize:'14px'}}><b>Doctor Name: </b>{this.doctor}</h4> 
+                                <h4 style={{fontSize:'14px'}}><b>Specialist: </b>{this.specialist}</h4>
+                                <h4 style={{fontSize:'14px'}}><b>Degree: </b>{this.degree}</h4>
+                                <h4 style={{fontSize:'14px'}}><b>Age: </b>{this.age}</h4>
+                                <h4 style={{fontSize:'14px'}}><b>Phone: </b>{this.phone}</h4>                                
+                                <h4 style={{fontSize:'14px'}}><b>Clinicfees: </b>{this.clinicfees}</h4>
+                                <h4 style={{fontSize:'14px'}}><b>Street: </b>{this.street} </h4>
+                                <h4 style={{fontSize:'14px'}}><b>Area: </b>{this.area} </h4>
+                                <h4 style={{fontSize:'14px'}}><b>City: </b>{this.city}</h4>
                                 {/* <AuthorizedRoute permission={this.state.user === 'patient' ? true : false }  path="/slotbook" exact strict 
                                  component={Slotbook} patientId={this.state.key}  />  */}
                                 {/* <AuthorizedComponent
@@ -149,14 +182,21 @@ class TempPage extends Component {
                                         
         </React.Fragment>     
             
-        } 
-        <div>
-        { this.state.bookSlot && <SendProps doctorName={this.props.computedMatch.params.id} patientId={this.props.patientId} searchDate={this.state.date}
-            timeGone={this.timeGone}
-        />}
-        </div>
-    </div>
-    );
+            </div>
+
+        )
+    }
+    else{
+       
+       return(<div>
+            <Spinner color='primary'></Spinner>
+        </div>)
+
+    }
+
+    
+
+
   }
 }
 
