d9dd45989a9408287d293b30e28b7d86a4239c59
Jeet Patel <Jeetadaiict@gmail.com>
2019-04-17 18:02:35 +0530

    prescription done
    History.js done

--- src/Components/Precripaton/History.js
+++ src/Components/Precripaton/History.js
@@ -94,34 +94,74 @@ import FlipMove from "react-flip-move";
 
         let arrayofhistory=this.state.prescription.map(prescription=>{
           return (
-            <div href="#" className="list-group-item ">
-              <h4 className="list-group-item-heading">
-                {prescription.clinic}hiiiiiiiiii
-              </h4>
-              <p className="list-group-item-text">
-                <text style={{color:'blue'}}> Time:&nbsp; &nbsp; &nbsp;  </text>
-                {new Date(prescription.time).toString()}
-              </p>
-              <p className="list-group-item-text">
-                <text style={{ color: 'blue' }}>Description:&nbsp; &nbsp; &nbsp;</text>
-                {prescription.description}
-              </p>
-              <a
-                href={prescription.link}
-                className="list-group-item-text"
-                target="_blank"
-              >
-                <button className="download">Download</button>
-              </a>
+
+
+            <div className="d-flex justify-content-center">
+            <div  style={{width:'50%'}}   style={{border:"3px solid grey",marginTop:'1%',borderRadius:'4%',padding:'0.5%',backgroundColor:'#254e58',borderBottomLeftRadius:'4%' }}>
+            {/* <form style={{border:"3px solid grey",marginTop:'1%',borderRadius:'4%',padding:'0.5%',backgroundColor:'#254e58',borderBottomLeftRadius:'4%' }}> */}
+            <div className='list-group'  style={{border:"3px solid grey"}}>
+            <div className='list-group-item' style={{backgroundColor:'#f1f1f1'}}>
+            <h2 style={{textAlign:"center"}}>{prescription.clinic}</h2>
+
+           <h4 style={{fontSize:'14px'}}><b>Date and Time: </b>{new Date(prescription.time).toString()}</h4> 
+            <h4 style={{fontSize:'14px'}}><b>Description: </b>{prescription.description}</h4>
+            
+            
+            <div className="d-flex justify-content-between" style={{marginTop:'2%'}}>
+            <div className="d-flex justify-content-start">
+
+            <a href={prescription.link}  target="_blank">
+             <button className="btn btn" style={{color:'white' ,borderRadius:'5%',height:"70%",borderEndStartRadius:'5%',backgroundColor:"#3aafa9"}}   >Download<i style={{marginLeft:'10%'}} className={'fas fa-download'} color="green"/></button>
+            </a>
+            {/* <a
+                 href={prescription.link}
+                 className="list-group-item-text"
+                 target="_blank"
+               >
+                 <button className="download">Download</button>
+               </a> */}
+             </div>
+             </div>
+            
+            </div>                                
             </div>
+            {/* </form> */}
+            </div>
+            </div>
+
+
+//hello
+
+            // <div href="#" className="list-group-item ">
+            //   <h4 className="list-group-item-heading">
+            //     {prescription.clinic}hiiiiiiiiii
+            //   </h4>
+            //   <p className="list-group-item-text">
+            //     <text style={{color:'blue'}}> Time:&nbsp; &nbsp; &nbsp;  </text>
+            //     {new Date(prescription.time).toString()}
+            //   </p>
+            //   <p className="list-group-item-text">
+            //     <text style={{ color: 'blue' }}>Description:&nbsp; &nbsp; &nbsp;</text>
+            //     {prescription.description}
+            //   </p>
+            //   <a
+            //     href={prescription.link}
+            //     className="list-group-item-text"
+            //     target="_blank"
+            //   >
+            //     <button className="download">Download</button>
+            //   </a>
+            // </div>
           );
         })
       
     return (
       <div>
-        <h2 style={{ color: "red", textAlign: "center" }}>
-          Past Appointments
-        </h2>
+        <div className="d-flex justify-content-center">
+                <div  className="d-flex justify-content-center" style={{marginTop:'1%'}}>
+                <h3><button className="btn btn" style={{color:'white',backgroundColor:'#116466',height:'70%',marginBottom:'10%',fontSize:'70%',padding:'1%',marginTop:'2%'}}><b>Prescription</b></button></h3>
+                </div>
+            </div>
         <FlipMove duration={500} easing="cubic-bezier(0,0,1,1)">
           {arrayofhistory}
         </FlipMove>
